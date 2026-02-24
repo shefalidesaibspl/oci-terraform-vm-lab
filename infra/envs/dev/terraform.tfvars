ceb51a7289ee1a5b375b8c31b1f2e362ad70f547
shefalidesaibspl <shefali.desai@beyondatagroup.com>
2026-02-24 10:03:08 +0530

    Fix tfvars (ssh key + OS selection) and auto-select image

--- infra/envs/dev/terraform.tfvars
+++ infra/envs/dev/terraform.tfvars
@@ -1,1 +1,23 @@
+region = "ap-mumbai-1"
+
+compartment_ocid = "ocid1.compartment.oc1..aaaaaaaaskt46d454ryspe4h2g7u25obrajosnnbldpm5ynye3qqt2n4gzlq"
+subnet_ocid      = "ocid1.subnet.oc1.ap-mumbai-1.aaaaaaaasij7qlrg73ebgcwmveduirnmuab6nub2wpgvxvbaxo4anlivmt2q"
+
+availability_domain = "GjHv:AP-MUMBAI-1-AD-1"
+
 instance_display_name = "devops-lab-vm-01-test"
+
+shape         = "VM.Standard.E4.Flex"
+ocpus         = 1
+memory_in_gbs = 6
+
+ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOib2dZknOdOFJIXHjTPDar8u4bCNu5mvDXN+rhRaFed oci-devops-lab"
+
+operating_system         = "Canonical Ubuntu"
+operating_system_version = "22.04"
+
+freeform_tags = {
+  owner       = "shefali"
+  purpose     = "devops-lab"
+  cost_center = "learning"
+}
---
+++ infra/variables.tf
@@ -0,0 +1,36 @@
+variable "tenancy_ocid" { type = string }
+variable "user_ocid" { type = string }
+variable "fingerprint" { type = string }
+variable "private_key" { type = string }
+
+variable "region" { type = string }
+
+variable "compartment_ocid" { type = string }
+variable "subnet_ocid" { type = string }
+
+variable "availability_domain" {
+  type        = string
+  description = "Example: kIdk:AP-MUMBAI-1-AD-1"
+}
+
+variable "instance_display_name" { type = string }
+
+variable "shape" { type = string }
+variable "ocpus" { type = number }
+variable "memory_in_gbs" { type = number }
+
+variable "ssh_public_key" { type = string }
+
+variable "operating_system" {
+  type        = string
+  description = "Ubuntu or Oracle Linux"
+}
+
+variable "operating_system_version" {
+  type        = string
+  description = "Example: 22.04 for Ubuntu, 9 for Oracle Linux"
+}
+variable "freeform_tags" {
+  type    = map(string)
+  default = {}
+}
